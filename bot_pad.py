3e1e8db816b7356c717023f22ee6c6aa95038721
asak200 <133242377+asak200@users.noreply.github.com>
2023-07-07 18:26:21 +0300

    Update bot_pad.py

--- bot_pad.py
+++ bot_pad.py
@@ -3,12 +3,13 @@ DIFFICULTY = 5
 
 screen = Screen()
 
+
 class BotPaddle:
     def __init__(self):
-        self.creat_paddle()
+        self.create_paddle()
         self.score = 0
 
-    def creat_paddle(self):
+    def create_paddle(self):
         self.paddle = Turtle('square')
         self.paddle.shapesize(stretch_wid=5, stretch_len=1)
         self.paddle.penup()
@@ -16,7 +17,7 @@ class BotPaddle:
         self.paddle.goto(350, 00)
 
     def defend(self, ball_pos, moves):
-        if 220 > ball_pos > -220 and moves % DIFFICULTY == 0:
+        if 190 > self.paddle.ycor() > -190 and moves % DIFFICULTY == 0:
             if ball_pos > self.paddle.ycor()+20:
                 self.paddle.sety(self.paddle.ycor()+20)
             elif ball_pos < self.paddle.ycor()-20:
